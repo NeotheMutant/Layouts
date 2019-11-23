2f89f7629edce12a0f9f1cd2e4abffe78f98b16f
Gaurav <gaurav.kumar.kashodhan@gmail.com>
2019-11-23 21:32:43 +0530

    "new layout image filter layout"

--- app/src/main/java/com/example/layouts/ImageFilterActivity.kt
+++ app/src/main/java/com/example/layouts/ImageFilterActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class ImageFilterActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imagefilter)
 
 
 
